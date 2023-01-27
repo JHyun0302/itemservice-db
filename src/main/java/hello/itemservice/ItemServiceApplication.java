3acfd95270ac1a6e0589f447e98afa2eb41a5673
jaehyun <wogustkfkd11@gmail.com>
2023-01-27 21:45:35 +0900

    임베디드 모드에서는 직접 만든 DataSource 쓸 필요 없음

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,16 +2,22 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 
 //@Import(MemoryConfig.class) //앞서 설정한 MemoryConfig를 설정 파일로 사용
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
+@Slf4j
 @Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web") //경로상 하위에 있는 controller만 컴포넌트 스캔을 사용, 나머지는 수동 등록
 public class ItemServiceApplication {
@@ -26,4 +32,18 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
+	/**
+	 * 임베디드 모드: DB를 이용하는데 마치 자바 메모리 방식처럼 application 종료하면 DB에 저장된 내용 삭제됨!
+	 */
+/*	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
+	}*/
 }
