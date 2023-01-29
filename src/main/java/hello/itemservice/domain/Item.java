99290d13316480a6f1661a7f3743a9abde988896
jaehyun <wogustkfkd11@gmail.com>
2023-01-29 22:54:27 +0900

    JPA 설정 추가

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,16 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity //JPA entity
+//@Table(name = "item") //객체명 == table 이름, 생략가능
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //PK, DB에서 값을 넣어주는 전략임!
     private Long id;
-
+    @Column(name = "item_name", length = 10) //DB의 "item_name"과 바인딩 (column == 변수명인 경우, 생략 가능)
     private String itemName;
     private Integer price;
     private Integer quantity;
